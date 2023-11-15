def119e3d1b3614666afd2a0b6c409aa10f093a9
Quan Tran <88959861+Quan987@users.noreply.github.com>
2023-11-15 15:11:17 -0500

    objective-c

---
+++ objective-c/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project-Summary</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <header id="header">
+        <div class="header__box">
+            <div class="header__logo"></div>
+            <div id="title">Team 2 - Web Development Summary </div>
+            <nav class="header__navbar">
+                <a href="https://csds.gsu.edu/" target="_self" rel="noopener noreferrer" class="header__link">Home</a>
+                <a href="../php/index.php" target="_self" rel="noopener noreferrer" class="header__link">Game</a>
+            </nav>
+        </div>  
+    </header>
+    <div id="main">
+        <div id="content">
+            <!-- <img id="icon" src="./pizza3.png" alt="pizza-img"> -->
+            <ul class="content__group">
+                <li>Leader Name: Quan Tran </li>
+                <li>Project Title: Who Want to be a Millionares</li>
+                <li>Project Description: Using HTML, CSS, PHP and animation transitions, we created a beautifully styled and dynamic website which allow user to play the 
+                    game Who Wants to Be a Millionaire?</li>
+                <li>Team Members: Nadia Trigoso, Isaac Harris, Bach Chiu, Aryan Asalkhou, Quan Tran
+                    <ul class="content__team-member">
+                        <li>Nadia Trigoso: Responsible for project front-page and HTML/PHP programming</li>
+                        <li>Isaac Harris: Responsible for main-project game mechanic and HTML/PHP programming </li>
+                        <li>Bach Chiu: Responsible for game mechanic and data of project and HTML/PHP programming</li>
+                        <li>Aryan Asalkhou: Responsible for Powerpoint slide, overall design and (HTML/PHP) programming</li>
+                        <li>Quan Tran: Reponsible for overall project framework and login page, and team coordination</li>
+                        <li>All Member: Responsible for CSS, presentation, and video</li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="copyright">
+		</p>Copyright &copy; 2023 
+		<br>Property of Isaac Harris, Bach Chiu, Nadia Trigoso, Aryan Asalkhou, Quan Tran
+		<br>Georgia State University - Web Development
+	</div>
+</body>
+</html>
